ba8af04ae7ef2918cf36c3995ba78d3acf845069
mngethe <njeringethe@gmail.com>
2023-02-10 09:52:04 +0300

    initial commit

---
+++ src/login/LoginComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoginComponent.css"
+
+function LoginComponent(){
+    return (
+        <div>
+            <p>
+                Login Component
+            </p>
+        </div>
+    )
+}
+export default LoginComponent
